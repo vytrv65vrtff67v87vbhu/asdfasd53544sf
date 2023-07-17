c8da47c60811749a72a9ea5b06df690b46ee7989
Mirko Ferrati <mirko.ferrati@gmail.com>
2023-07-17 19:35:11 +0200

    asfad

---
+++ snap/local/log_stats.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Save off parameters, the sourced setup scripts may manipulate them.
+original_args=("$@")
+
+# Get the current usage of CPU and memory
+cpuUsage=$(top -bn1 | awk '/Cpu/ { print $2}')
+memUsage=$(free -m | awk '/Mem/{print $3}')
+
+# Print the usage
+echo "CPU Usage: $cpuUsage%"
+echo "Memory Usage: $memUsage MB"
+df -H
+  
+exec "${original_args[@]}"
